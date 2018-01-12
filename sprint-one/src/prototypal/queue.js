ea54362f7946683f51fd22a26ee91fb2c37d0a02
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-01-12 11:26:13 -0600

    finished prototypal queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.add = 1;
+  someInstance.remove = 1;
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.add] = value;
+    this.add++;
+  },
+  dequeue: function(){
+    var newValue = this.storage[this.remove];
+    delete this.storage[this.remove];
+    this.remove++;
+    return newValue;
+  },
+  size: function(){
+    if(this.add - this.remove > 0){
+      return this.add - this.remove;
+    } else {
+      return 0;
+    }
+  }
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -2,27 +2,25 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(stackMethods);
-  someInstance.add = 1;
-  someInstance.shift = 1;
+  someInstance.counter = 0;
   someInstance.storage = {};
-
   return someInstance;
 };
 
 var stackMethods = {
-  push: fuction(data){
-    this.storage[this.shift] = data;
-    this.shift++;
+  push: function(value){
+    this.storage[this.counter] = value;
+    this.counter++;
   },
   pop: function(){
-    if(this.shift > 0){
-      this.shift--;
-      var newData = this.storage[this.shift];
-      delete this.storage[this.shift];
-      return newData;
+    if(this.counter > 0){
+      this.counter--;
+      var newValue = this.storage[this.counter];
+      delete this.storage[this.counter];
+      return newValue;
     }
-  },
-  size: function(){
-    return this.shift;
+    },
+    size: function(){
+      return this.counter;
   }
 };
