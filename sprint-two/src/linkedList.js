bab2d85654631e57f1547d0dc9b52c5923d8bb10
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-01-13 17:42:37 -0600

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = list.head;
+    }else{
+      list.tail.next = newNode;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var current = list.head;
+    list.head = list.head.next;
+    return current.value;
   };
 
   list.contains = function(target) {
+    var existingNode = list.head;
+    while (existingNode){
+      if(existingNode.value === target){
+      return true;
+    }
+      existingNode = existingNode.next;
+    }
+    return false;
   };
 
   return list;
