7fbfa400e9f7e585912d3cba063df598617004a6
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-01-15 20:11:49 -0600

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,29 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = [];  // fix me
-  _.extend(newTree, treeMethods)
-  return newTree;
-};
+  Object.assign(newTree,treeMethods);
+   newTree.children = [];
+   return newTree;
+  };
 
-var treeMethods = {};
+  var treeMethods = {};
 
-treeMethods.addChild = function(value) {
-  newTree.push(value);
-};
+  treeMethods.addChild = function(value) {
+   var newChild = Tree(value);
+   this.children.push(newChild);
+  };
 
-treeMethods.contains = function(target) {
-};
+  treeMethods.contains = function(target) {
+   if (this.value === target) {
+     return true;
+   }
+   for (var i = 0; i < this.children.length; i++) {
+     if (this.children[i].contains(target)) {
+       return true;
+     }
+   }
+   return false;
+  };
 
 
 
