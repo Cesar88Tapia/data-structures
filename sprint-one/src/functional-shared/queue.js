87cbd55a7a5cbfcdb40beaaccef33afbc5c4271e
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-07-18 19:48:56 -0500

    finished functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,13 +1,35 @@
 // Hey! Rewrite in the new style. Your code will wind up looking very similar,
 // but try not not reference your old code in writing the new style.
 
+
 var Queue = function() {
-  var arrange = {
-    entries: 0,
+  var serving = {
+    sum: 1,
+    change: 1,
     storage: {}
   }
-  _.extend(arrange, stackMethods)
-  return arrange;
+
+  _.extend(serving, queueMethods)
+
+  return serving;
 };
 
-  var queueMethods = {};
+  var queueMethods = {
+    enqueue: function(gain){
+      this.storage[this.sum] = gain;
+      this.sum++;
+    },
+    dequeue: function(){
+      var val = this.storage[this.change];
+      delete this.storage[this.change];
+      this.change++;
+      return val;
+    },
+    size: function(){
+      if(this.sum - this.change > 0){
+        return this.sum - this.change;
+      } else {
+        return 0;
+      }
+    }
+  };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,89 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var arrange = {
+    entry: 0,
+    storage: {}
+  }
+
+  _.extend(arrange, stackMethods);
+
+  return arrange;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(data){
+    this.storage[this.entry] = data;
+    this.entry++;
+  },
+  pop: function(){
+    this.entry--;
+    var newData = this.storage[this.entry];
+    delete this.storage[this.entry];
+    return newData;
+  },
+  size: function(){
+    if(this.entry > 0){
+      return this.entry;
+    } else {
+      return 0;
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+var stacking = {
+  ins: 0,
+  storage: {}
+}
+_.extend(stacking, stackMethods)
+return stacking;
+};
+
+var stackMethods = {
+  push: function(data){
+    this.storage[this.ins] = data;
+    this.ins++;
+  },
+  pop: function(){
+    this.ins--;
+    var newData = this.storage[this.ins];
+    delete this.storage[this.ins];
+    return newData;
+  },
+  size: function(){
+    if(this.ins > 0){
+      return this.ins;
+    }else{
+      return 0;
+    }
+  }
+};*/
